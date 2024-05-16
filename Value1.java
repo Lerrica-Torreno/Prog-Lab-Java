bfb314ca2460e1905b86c1403fe8144516d2fb60
Lerrica Torreno <155938425+Lerrica-Torreno@users.noreply.github.com>
2024-05-16 12:19:25 +0800

    first commit

---
+++ Value1.java
@@ -0,0 +1,14 @@
+/* Name: TORRENO, Lerrica Jeremy S.
+ * Course/Year: BSCS-DS/1st year 
+ * Date: April 20, 2024 
+ * Work title: Value1 (if statement) 
+ */
+
+public class Value1 {
+    public static void main(String[] args) {
+        
+        int b = 15;
+        if (b %2 != 0)
+        System.out.println("This number is not an even number");
+    }
+}
